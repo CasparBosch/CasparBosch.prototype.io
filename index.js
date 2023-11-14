9f6a72e109599b2ee685c87fdfac36e2e1e86a98
CasparBosch <74655948+CasparBosch@users.noreply.github.com>
2023-11-14 13:41:36 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-async function displayResults() {
+setInterval(selectScreen, 300000);
+
+function selectScreen() {
   const d = new Date();
   let hour = d.getHours();
-  let minutes = d.getMinutes();
-  let day = d.getDay();
-  const currentDay = [
-    "sunday",
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday",
-  ][day].toLowerCase();
 
-  if (hour > "09" && hour < "12") {
+  if (hour < 12) {
+    
     window.location.href = './ochtend.html';
     console.log('./ochtend.html');
-  } else if (hour > "12" && hour < "15") {
+  } else if (hour >= 12 && hour < 15) {
     window.location.href = './middag.html';
-  } else if (hour > "15" && hour < "17") {
+  } else {
     window.location.href = './avond.html';
   }
 }
 
-displayResults();
-
-console.log(displayResults());
